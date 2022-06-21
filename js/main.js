b967e0042e73514eec11c7a47bdf55b49b67b760
MinervaD <minervadiago@gmail.com>
2022-06-21 18:55:15 -0500

    feat: add search input

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 
-if (window.performance.navigation.type == 1) {
-      location.href = '../html/introScreen.html';
-}
+// if (window.performance.navigation.type == 1) {
+//       location.href = '../html/introScreen.html';
+// }
 var URI = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=90/'
 var URI2 = 'https://pokeapi.co/api/v2/pokemon/'
 
+document.querySelector("#search").addEventListener("keypress", function (evt) {
+      if(event.keyCode === 13){
+            let pokemon = event.target.value
+            //console.log(pokemon)
+            createCards(1,1, pokemon)
+      }
+});
+
 const btnClicked = (numero) => {
-      console.log('BOTON '+numero+' presionado')
+      document.querySelector('#search').value = ('')
+      console.log('BOTON ' + numero + ' presionado')
 
       btn_1 = document.querySelector('#btn_1')
       btn_1.classList.remove('is-focused')
 
-      switch(numero){
+      switch (numero) {
             case 1: createCards(1, 54); break
             case 2: createCards(55, 108); break
             case 3: createCards(109, 162); break
@@ -32,151 +41,186 @@ const btnClicked = (numero) => {
       }
 }
 
-const createCards = (numInicio = 1, numFinal = 54) => {
+const createCards = (numInicio = 1, numFinal = 54, namePokemon = '') => {
 
-    for(var i=numInicio;i<=numFinal;i++){
+      document.querySelector('#listado').innerHTML = ''
 
-        let div = document.createElement('div');
-        div.classList.add('column','is-2');
+      for (var i = numInicio; i <= numFinal; i++) {
 
-        let card = document.createElement('div');
-        let hTitle= document.createElement("h2")
-        let p1 = document.createElement("p")   
-        let figure = document.createElement("figure")
-        let cardImage =  document.createElement("div")
-        cardImage.classList.add("card-image")
-        
-        let cardContent= document.createElement("div")
-        cardContent.classList.add("card-content")
-    
-        let img = document.createElement("img")
-        img.classList.add('sizeImg')
+            let buttons = document.querySelector('#buttons')
+            //buttons.classList.remove('is-hidden')
 
-        let p2 = document.createElement("p")
-        let p3 = document.createElement("p")
-        
-        const fillCards = async () => {
-            try {   
-                  const infoPokemon = await fetch(URI2+[i])
-                  const dataPokemon = await infoPokemon.json()
-                  //console.log(dataPokemon)
-                  img.setAttribute("src",dataPokemon.sprites.other.home.front_default)
+            let div = document.createElement('div');
+            div.classList.add('column', 'is-2');
 
-                  card.classList.add('card','has-background-white',borderColor(dataPokemon.base_experience))
-                  figure.classList.add(bgColor(dataPokemon.base_experience))
+            let card = document.createElement('div');
+            let hTitle = document.createElement("h2")
+            let p1 = document.createElement("p")
+            let figure = document.createElement("figure")
+            let cardImage = document.createElement("div")
+            cardImage.classList.add("card-image")
 
-                  namePoke = dataPokemon.name
-                  hTitle.innerText=namePoke.charAt(0).toUpperCase()+namePoke.slice(1)
-                  hTitle.classList.add('is-4','titleCard',titleCard(namePoke.length), "has-text-primary")
+            let cardContent = document.createElement("div")
+            cardContent.classList.add("card-content")
 
-                  p1.classList.add('idPokemon', idPokemon(dataPokemon.id))
-                  p1.innerHTML="#"+dataPokemon.id
-                  p3.innerHTML=dataPokemon.base_experience+' EXP.🏹'
-                  p3.classList.add('p3')
+            let img = document.createElement("img")
+            img.classList.add('sizeImg')
 
-            } catch (error) {
-                  console.log(error)
-                  hTitle.innerText='ERROR'
-                  p2.classList.add('ml-3','has-text-white')
-                  p2.innerHTML='404 - NOT FOUND'         
+            let p2 = document.createElement("p")
+            let p3 = document.createElement("p")
+            let p4 = document.createElement("p")
+            p4.classList.add('p4')
+
+            /* let espada = document.createElement('img')
+            espada.classList.add('espada')
+            espada.setAttribute('src','https://i.pinimg.com/originals/53/f7/34/53f7341cb281735cd89dc52dcd262d31.png') */
+
+           
+                  
+
+            const fillCards = async () => {
+                  
+                  try {
+                        
+                        if(namePokemon.length != 0) {  
+                              url = URI2+namePokemon
+                              btn_1.classList.remove('is-focused')
+                              //buttons.classList.add('is-hidden')
+                        }else if(namePokemon.length == 0){
+                              url = URI2+[i]
+                        }
+                        
+                        const infoPokemon = await fetch(url)
+                        const dataPokemon = await infoPokemon.json()
+                        //console.log(dataPokemon)
+                        img.setAttribute("src", dataPokemon.sprites.other.home.front_default)
+
+                        card.classList.add('card', 'has-background-white', borderColor(dataPokemon.base_experience))
+                        figure.classList.add(bgColor(dataPokemon.base_experience))
+
+                        namePoke = dataPokemon.name
+                        hTitle.innerText = namePoke.charAt(0).toUpperCase() + namePoke.slice(1)
+                        hTitle.classList.add('is-4', 'titleCard', titleCard(namePoke.length), "has-text-primary")
+
+                        p1.classList.add('idPokemon', idPokemon(dataPokemon.id))
+                        p1.innerHTML = "#" + dataPokemon.id
+                        p3.innerHTML = '🧡' + dataPokemon.base_experience
+                        p3.classList.add('p3')
+                        p4.innerHTML = 'EXP'
+
+
+                  } catch (error) {
+                        console.log(error)
+                        hTitle.innerText = 'ERROR 404 😢'
+                        hTitle.classList.add('has-text-white','is-5')
+                        card.classList.add('has-background-danger')
+                        p2.classList.add('ml-3','p-1', 'has-text-white')
+                        p2.innerHTML = 'POKEMON NOT FOUND'
+                  }
             }
-        }
-        fillCards()
+            fillCards()
 
-        cardContent.append(hTitle)
-        figure.append(img)
-        cardImage.append(figure)
-        cardContent.append(p1)
-        cardContent.append(p2)
-        cardContent.append(p3)
-        
-        card.append(cardImage)
-        card.append(cardContent)
-        div.append(card)
+            cardContent.append(hTitle)
+            figure.append(img)
+            cardImage.append(figure)
+            cardContent.append(p1)
+            cardContent.append(p2)
+            cardContent.append(p3)
+            cardContent.append(p4)
+            //cardContent.append(espada)
 
-        document.querySelector("#listado").append(div)
-        
-      //   card.addEventListener("click", popup)
-    }
+            card.append(cardImage)
+            card.append(cardContent)
+            div.append(card)
+
+            document.querySelector("#listado").append(div)
+
+            //   card.addEventListener("click", popup)
+      }
 }
 
 const bgColor = colorNumber => {
       let colorBg
-      if(colorNumber >= 0 && colorNumber < 50) {
+      if (colorNumber >= 0 && colorNumber < 50) {
             colorBg = 'agua-light'
       }
-      else if(colorNumber >= 51 && colorNumber < 100) {
+      else if (colorNumber >= 51 && colorNumber < 100) {
             colorBg = 'purple-light'
       }
-      else if(colorNumber >= 101 && colorNumber < 150) {
+      else if (colorNumber >= 101 && colorNumber < 150) {
             colorBg = 'orange-light'
       }
-      else if(colorNumber >= 151 && colorNumber < 200) {
+      else if (colorNumber >= 151 && colorNumber < 200) {
             colorBg = 'green-light'
       }
-      else if(colorNumber >= 201 && colorNumber < 300) {
+      else if (colorNumber >= 201 && colorNumber < 300) {
             colorBg = 'blue-light'
       }
-      else if(colorNumber >= 301 && colorNumber < 350) {
+      else if (colorNumber >= 301 && colorNumber < 350) {
             colorBg = 'red-light'
       }
-      else{
+      else {
             colorBg = 'black-light'
       }
       return colorBg
 }
 const borderColor = colorNumber => {
       let borderColor
-      if(colorNumber >= 0 && colorNumber < 50) {
+      if (colorNumber >= 0 && colorNumber < 50) {
             borderColor = 'border-agua-light'
       }
-      else if(colorNumber >= 51 && colorNumber < 100) {
+      else if (colorNumber >= 51 && colorNumber < 100) {
             borderColor = 'border-purple-light'
       }
-      else if(colorNumber >= 101 && colorNumber < 150) {
+      else if (colorNumber >= 101 && colorNumber < 150) {
             borderColor = 'border-orange-light'
       }
-      else if(colorNumber >= 151 && colorNumber < 200) {
+      else if (colorNumber >= 151 && colorNumber < 200) {
             borderColor = 'border-green-light'
       }
-      else if(colorNumber >= 201 && colorNumber < 300) {
+      else if (colorNumber >= 201 && colorNumber < 300) {
             borderColor = 'border-blue-light'
       }
-      else if(colorNumber >= 301 && colorNumber < 350) {
+      else if (colorNumber >= 301 && colorNumber < 350) {
             borderColor = 'border-red-light'
       }
-      else{
+      else {
             borderColor = 'border-black-light'
       }
       return borderColor
 }
-const idPokemon = (id) =>{
+const idPokemon = (id) => {
       let positionId
 
-      if(id >= 1 && id <=9){
+      if (id >= 1 && id <= 9) {
             positionId = 'idPokemon0-9'
       }
-      else if(id >= 10 && id <=99){
+      else if (id >= 10 && id <= 99) {
             positionId = 'idPokemon10-99'
       }
-      else{
-            positionId='idPokemon100'
+      else {
+            positionId = 'idPokemon100'
       }
       return positionId
 }
-const titleCard = (nameLength) =>{
+const titleCard = (nameLength) => {
       let classCss
 
-      if(nameLength <= 10) classCss= 'titleCard-10letters'
+      if (nameLength <= 10) classCss = 'titleCard-10letters'
 
-      else if( nameLength<=14) {
+      else if (nameLength <= 14) {
             classCss = 'titleCard-14letters'
       }
-      else if ( nameLength<=18){
+      else if (nameLength <= 18) {
             classCss = 'titleCard-18Letters'
-      }else{
-            classCss ='titleCard-19letters'
+      } else {
+            classCss = 'titleCard-19letters'
       }
       return classCss
 }
 createCards()
+
+callCreateCards =() =>{
+      document.querySelector('#search').value = ('')
+      createCards()
+}
